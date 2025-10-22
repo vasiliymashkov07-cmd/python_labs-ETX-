310ba26127e1249c6fdeae553f430b46da303968
vasiliymashkov07-cmd <vasiliymashkov07@gmail.com>
2025-10-22 21:37:02 +0300

    Final

--- README.md
+++ README.md
@@ -166,3 +166,40 @@ print(col_sums([[0, 0], [0, 0]]))
 print(col_sums([[1, 2], [3]]))
 ```
 ![](/images/lab%2002/matrix.png)
+
+
+# Задание 3 - tuples.py
+```python
+def errorcheckandformat(fio: str, group: str, gpa: float) -> tuple:
+    if not isinstance(fio, str):
+        raise TypeError("Строки нету")
+    if not isinstance(group, str):
+        raise TypeError("Строки нету")
+    if not isinstance(gpa, (float, int)):
+        raise TypeError("Тип float или int не найден")
+    
+    nameandsurname = fio.strip().split() #Удаляем пробелы и разбиваем
+    nameandsurname = [x.capitalize() for x in  nameandsurname] #1-буква с заглавной, остальные с маленькой
+
+
+    def format(parts):
+        username = parts[0]
+        first_initial = parts[1][0].upper() + '.'
+        if len(parts) > 2:
+            secondinitial = parts[2][0].upper() + '.'
+        else: secondinitial = ''
+        return f'{username} {first_initial}{secondinitial}'
+    
+    new_fio = format(nameandsurname)
+    new_gpa = f'{gpa:.2f}' #Округляем до 2 знаков после запятой
+
+    return (new_fio,group,new_gpa)
+
+print(errorcheckandformat("Иванов Иван Иванович", " гр. BIVT-25", 4.6))
+print(errorcheckandformat("Петров Пётр", " гр. IKBO-12", 5.0))
+print(errorcheckandformat("Петров Пётр Петрович", " гр. IKBO-12", 5.0))
+print(errorcheckandformat("  сидорова  анна   сергеевна ", " гр. ABB-01", 3.999))
+```
+![](/images/lab%2002/tuples.png)
+
+
--- src/lab 02/tuples.py
+++ src/lab 02/tuples.py
@@ -1,9 +1,40 @@
-def data(fio: str, group: str, gpa: float) -> tuple:
-    if not isinstance(fio,str):
-        return ValueError('fio - Необходим тип str')
-    if not isinstance(group,str):
-        return ValueError('group - Необходим тип str')
-    if not isinstance(gpa,float,int):
-        return ValueError('gpa - Необходимы типы float или int')
+def errorcheckandformat(fio: str, group: str, gpa: float) -> tuple:
+    if not isinstance(fio, str):
+        raise TypeError("Строки нету")
+    if not isinstance(group, str):
+        raise TypeError("Строки нету")
+    if not isinstance(gpa, (float, int)):
+        raise TypeError("Тип float или int не найден")
     
-    return ((changing I: f'{I[0].capitalize()} {I[1][0].upper()}.{I[2][0].upper + '.'}    '))
+    nameandsurname = fio.strip().split() #Удаляем пробелы и разбиваем
+    nameandsurname = [x.capitalize() for x in  nameandsurname] #1-буква с заглавной, остальные с маленькой
+
+
+    def format(parts):
+        username = parts[0]
+        first_initial = parts[1][0].upper() + '.'
+        if len(parts) > 2:
+            secondinitial = parts[2][0].upper() + '.'
+        else: secondinitial = ''
+        return f'{username} {first_initial}{secondinitial}'
+    
+    new_fio = format(nameandsurname)
+    new_gpa = f'{gpa:.2f}' #Округляем до 2 знаков после запятой
+
+    return (new_fio,group,new_gpa)
+
+print(errorcheckandformat("Иванов Иван Иванович", " гр. BIVT-25", 4.6))
+print(errorcheckandformat("Петров Пётр", " гр. IKBO-12", 5.0))
+print(errorcheckandformat("Петров Пётр Петрович", " гр. IKBO-12", 5.0))
+print(errorcheckandformat("  сидорова  анна   сергеевна ", " гр. ABB-01", 3.999))
+
+
+
+
+
+
+
+
+  
+
+ 
